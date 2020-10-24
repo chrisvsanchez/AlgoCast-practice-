1b41159766ab4d58a7af68f0954bfe34c8a0ae14
chrisvsanchez <chrisvsanchez@gmail.com>
2020-10-24 00:12:33 -0400

    solution complete

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,15 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let strArr = str.split("");
+  let result = [];
+  console.log(strArr);
+  for (let i = strArr.length - 1; i >= 0; i--) {
+    result.push(strArr[i]);
+  }
+  return result.join("");
+}
+console.log(reverse("dcba"));
 
 module.exports = reverse;
