b3077452ce46f72046f14b1cd9adb0e7823f329d
chrisvsanchez <chrisvsanchez@gmail.com>
2020-10-25 00:38:47 -0400

    maxChar solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+  for (let char in charMap) {
+    console.log("this is char", char);
+
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  console.log(charMap);
+  return maxChar;
+}
 
 module.exports = maxChar;
